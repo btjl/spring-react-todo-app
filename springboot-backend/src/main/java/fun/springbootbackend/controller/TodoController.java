a4c3ae191555dc7781209ff9d8cb98f86ca0ad16
Brennan <tanjialebrennan@gmail.com>
2022-06-20 15:40:14 +0800

    Create Todo controller

---
+++ springboot-backend/src/main/java/fun/springbootbackend/controller/TodoController.java
@@ -0,0 +1,25 @@
+package fun.springbootbackend.controller;
+
+import fun.springbootbackend.entity.Todo;
+import fun.springbootbackend.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+}
