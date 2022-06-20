33d1939177cd70dd4406a6dcab251160aba8c218
Brennan <tanjialebrennan@gmail.com>
2022-06-21 00:48:14 +0800

    Update App.js with Header and Footer components

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import ListTodosComponent from './components/ListTodosComponent';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route exact path="/" element={<ListTodosComponent/>}/>
-          <Route path="/todos" element={<ListTodosComponent/>}/>
-        </Routes>
+    <div>
+      <HeaderComponent/>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<ListTodosComponent/>}/>
+            <Route path="/todos" element={<ListTodosComponent/>}/>
+          </Routes>
+        </div>
+        <FooterComponent/>
       </div>
     </BrowserRouter>
   );
