bf862bb933f156d6278f697e3de26a70c11b5450
Brennan <tanjialebrennan@gmail.com>
2022-06-22 18:17:50 +0800

    Update Todo controller with new POST method

--- springboot-backend/src/main/java/fun/springbootbackend/controller/TodoController.java
+++ springboot-backend/src/main/java/fun/springbootbackend/controller/TodoController.java
@@ -3,10 +3,7 @@ package fun.springbootbackend.controller;
 import fun.springbootbackend.entity.Todo;
 import fun.springbootbackend.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class TodoController {
     @GetMapping
     public List<Todo> getAllTodos() {
         return todoRepository.findAll();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
     }
 }
